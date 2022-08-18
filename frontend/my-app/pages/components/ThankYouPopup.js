16a82951441781255e2decf1471a6ccf6bdba30c
Okiki-Olugunna <92333005+Okiki-Olugunna@users.noreply.github.com>
2022-08-18 22:44:08 +0100

    changed the component name to ThankYouPopup

--- frontend/my-app/pages/components/ThankYouPopup.js
+++ frontend/my-app/pages/components/ThankYouPopup.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import "./Popup.css";
 
-export default function Popup(props) {
+export default function ThankYouPopup(props) {
   return props.trigger ? (
     <div className="popup">
       <div className="popup-inner">
