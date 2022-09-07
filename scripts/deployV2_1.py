e34a15622d0a65ba8ff085380f7d28c89a990f54
Okiki-Olugunna <92333005+Okiki-Olugunna@users.noreply.github.com>
2022-09-07 22:38:59 +0100

    updated deploy script to update the abi & address on each deployment

--- scripts/deployV2_1.py
+++ scripts/deployV2_1.py
@@ -1,17 +1,36 @@
 from brownie import BuyMeACoffeeV2_1, accounts, config
+import json
 
 
 def deploy():
     account = accounts.add(config["wallets"]["from_key"])
 
-    print("Deploying BuyMeACoffeeV2_1 contract ...")
+    print("Deploying BuyMeACoffeeV2_1 contract ...\n")
     contract = BuyMeACoffeeV2_1.deploy({"from": account})
-    print(f"Contract deployed to: {contract}")
-    # print(f"The contract abi: {contract.abi}")
+    print(f"Contract deployed to: {contract.address}\n")
+
+    print("Updating the abi file in the frontend...")
+
+    abi = contract.abi
+    json_object = json.dumps(abi)
+    with open("./frontend/my-app/utils/V2_1_abi.json", "w") as file:
+        file.write(json_object)
+
+    print("Successfully updated the ABI.\n")
+
+    print("Updating the contract address file in the frontend...\n")
+    
+    address = contract.address
+    with open("./frontend/my-app/utils/address.txt", "w") as file:
+        file.write(address)
+
+    print("Successfully updated the contract address.\n")
+
+    print("The deployed contract is ready to be interacted with. :) \n")
 
 
 def main():
     deploy()
-
+    
 
 # deployed at 0xE9D3AB08080d797DC6856fC476e2b9BcD778Af84
