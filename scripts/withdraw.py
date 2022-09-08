6d6780bbf6c8fe3029e73dfa3a48fe82dec3b0b6
Okiki-Olugunna <92333005+Okiki-Olugunna@users.noreply.github.com>
2022-09-08 16:58:52 +0100

    cleaned up the withdraw script

--- scripts/withdraw.py
+++ scripts/withdraw.py
@@ -2,201 +2,87 @@
 from brownie import accounts, config
 from web3 import Web3
 from eth_utils import from_wei
+from decimal import Decimal
 
-w3 = Web3(
-    Web3.HTTPProvider(
-        ""
-      # removed my HTTP provider link since I'm pushing to github 
-    )
+
+w3 = Web3(Web3.HTTPProvider(config["provider"]["http"]))
+
+with open("./contract_data/address.txt") as file_a:
+    contract_address = file_a.read()
+
+with open("./contract_data/abi.json") as file_b:
+    contract_abi = file_b.read()
+
+# initialising the contract instance
+contract_instance = w3.eth.contract(address=contract_address, abi=contract_abi)
+
+# getting erc20 token info
+with open("./contract_data/erc20_abi.json") as file_c:
+    a_weth_abi = file_c.read()
+
+# initialising the aWeth token
+a_weth_token = w3.eth.contract(
+    address="0x27B4692C93959048833f40702b22FE3578E77759", abi=a_weth_abi
 )
-
-contract_address = "0xE43841588D314D6Fe155dB1Fd6F7C9D5b71fAf08"
-contract_abi = [
-    {
-        "inputs": [],
-        "stateMutability": "nonpayable",
-        "type": "constructor",
-        "name": "constructor",
-    },
-    {
-        "anonymous": False,
-        "inputs": [
-            {
-                "indexed": False,
-                "internalType": "uint256",
-                "name": "_datetime",
-                "type": "uint256",
-            },
-            {
-                "indexed": True,
-                "internalType": "address",
-                "name": "_tipper",
-                "type": "address",
-            },
-            {
-                "indexed": False,
-                "internalType": "string",
-                "name": "_name",
-                "type": "string",
-            },
-            {
-                "indexed": False,
-                "internalType": "uint256",
-                "name": "_amount",
-                "type": "uint256",
-            },
-            {
-                "indexed": False,
-                "internalType": "string",
-                "name": "_message",
-                "type": "string",
-            },
-        ],
-        "name": "NewCoffeeBought",
-        "type": "event",
-    },
-    {
-        "anonymous": False,
-        "inputs": [
-            {
-                "indexed": False,
-                "internalType": "uint256",
-                "name": "_datetime",
-                "type": "uint256",
-            },
-            {
-                "indexed": False,
-                "internalType": "uint256",
-                "name": "_amount",
-                "type": "uint256",
-            },
-        ],
-        "name": "WithdrawnCoffees",
-        "type": "event",
-    },
-    {
-        "inputs": [],
-        "name": "AAVE_V3_POOL",
-        "outputs": [{"internalType": "contract IPool", "name": "", "type": "address"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [],
-        "name": "ETHDonations",
-        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [],
-        "name": "WETH_ATOKEN",
-        "outputs": [{"internalType": "contract IERC20", "name": "", "type": "address"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [],
-        "name": "WETH_GATEWAY",
-        "outputs": [
-            {"internalType": "contract IWETHGateway", "name": "", "type": "address"}
-        ],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [
-            {"internalType": "string", "name": "_name", "type": "string"},
-            {"internalType": "string", "name": "_message", "type": "string"},
-        ],
-        "name": "buyCoffeeWithETH",
-        "outputs": [],
-        "stateMutability": "payable",
-        "type": "function",
-    },
-    {
-        "inputs": [{"internalType": "address", "name": "_newOwner", "type": "address"}],
-        "name": "changeOwner",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function",
-    },
-    {
-        "inputs": [],
-        "name": "checkBalanceETH",
-        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [],
-        "name": "howMany",
-        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [{"internalType": "address", "name": "", "type": "address"}],
-        "name": "isKind",
-        "outputs": [{"internalType": "bool", "name": "", "type": "bool"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
-        "name": "kindPeople",
-        "outputs": [{"internalType": "address", "name": "", "type": "address"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [],
-        "name": "owner",
-        "outputs": [{"internalType": "address payable", "name": "", "type": "address"}],
-        "stateMutability": "view",
-        "type": "function",
-    },
-    {
-        "inputs": [],
-        "name": "withdrawETHAave",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function",
-    },
-]
 
 
 def main():
     # initialising my account
-    account = accounts.add(config["wallets"]["from_key"])
+    account = config["wallets"]["from_key"]["account"]
+    account_address = config["addresses"]["account_address"]
 
-    # intialising the contract
-    contract_instance = w3.eth.contract(address=contract_address, abi=contract_abi)
-    print(f"The coffee contract address is {contract_instance.address}")
+    print(f"The coffee contract address is {contract_instance.address}\n")
 
     # getting the contract balance
-    contract_balance = from_wei(
+    contract_balance = w3.fromWei(
         contract_instance.functions.ETHDonations().call(), "ether"
     )
-    print(f"The contract balance is {contract_balance} ether.")
+    print(f"The contract balance is {contract_balance} ether.\n")
 
     # checking my balance
-    my_balance = from_wei(w3.eth.get_balance(account.address), "ether")
-    print(f"Your current balance is {my_balance} ether.")
+    my_balance = w3.fromWei(w3.eth.get_balance(account_address), "ether")
+    print(f"Your current balance is {my_balance} ether.\n")
 
     # withdrawing
-    print("Withdrawing donations + yield...")
-    tx = contract_instance.functions.withdrawETHAave().transact(
-        {"from": account.address}
+    print("Withdrawing donations + yield...\n")
+
+    print("Getting the nonce...\n")
+    nonce = w3.eth.get_transaction_count(account_address)
+    print("Nonce obtained...\n")
+
+    print("Building the transaction...\n")
+    tx = contract_instance.functions.withdrawETHAave().buildTransaction(
+        {
+            "gasPrice": w3.eth.gas_price,
+            "gas": 500000,
+            "chainId": 5,
+            "from": account_address,
+            "nonce": nonce,
+        }
     )
-    tx.wait(2)
+    print("Transaction built.\n")
+
+    # signing and sending the transaction
+    print("Signing the transaction...\n")
+    signed_tx = w3.eth.account.sign_transaction(tx, account)
+
+    print("Sending the transaction...\n")
+    send_tx = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
+
+    print("Waiting for the transaction receipt...\n")
+    tx_receipt = w3.eth.wait_for_transaction_receipt(send_tx)
+
+    print("--------------------------------------------")
+    print(f"Transaction Receipt:\n{tx_receipt}")
+    print("--------------------------------------------\n")
+
     print("Successfully withdrawn.")
 
     # checking new balances
-    my_balance = from_wei(w3.eth.get_balance(account.address), "ether")
-    print(f"Your new balance is {my_balance} ether.")
+    my_balance = w3.fromWei(w3.eth.get_balance(account_address), "ether")
+    print(f"Your new balance is {my_balance} ether.\n")
 
-    new_contract_balance = from_wei(
+    new_contract_balance = w3.fromWei(
         contract_instance.functions.ETHDonations().call(), "ether"
     )
-    print(f"The coffee contract new balance is {new_contract_balance} ether")
+    print(f"The coffee contract new balance is {new_contract_balance} ether\n")
