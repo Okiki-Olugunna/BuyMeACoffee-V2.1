326a8b59d9c17efcce9156204bf5d009ffc6ed13
Okiki-Olugunna <92333005+Okiki-Olugunna@users.noreply.github.com>
2022-09-08 16:55:15 +0100

    commit

--- scripts/deployV2_1.py
+++ scripts/deployV2_1.py
@@ -9,19 +9,23 @@ def deploy():
     contract = BuyMeACoffeeV2_1.deploy({"from": account})
     print(f"Contract deployed to: {contract.address}\n")
 
-    print("Updating the abi file in the frontend...")
+    print("Updating the abi file in the frontend...\n")
 
     abi = contract.abi
     json_object = json.dumps(abi)
     with open("./frontend/my-app/utils/V2_1_abi.json", "w") as file:
         file.write(json_object)
+    with open("./contract_data/abi.json", "w") as file:
+        file.write(json_object)
 
     print("Successfully updated the ABI.\n")
 
     print("Updating the contract address file in the frontend...\n")
-    
+
     address = contract.address
     with open("./frontend/my-app/utils/address.txt", "w") as file:
+        file.write(address)
+    with open("contract_data/address.txt", "w") as file:
         file.write(address)
 
     print("Successfully updated the contract address.\n")
@@ -31,6 +35,6 @@ def deploy():
 
 def main():
     deploy()
-    
 
+    
 # deployed at 0xE9D3AB08080d797DC6856fC476e2b9BcD778Af84
