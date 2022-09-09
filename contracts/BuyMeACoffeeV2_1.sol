3b8e56965b0b1e8125a2d58ebda01ff1c2788131
Okiki-Olugunna <92333005+Okiki-Olugunna@users.noreply.github.com>
2022-09-09 13:37:49 +0100

    added natspec notation

--- contracts/BuyMeACoffeeV2_1.sol
+++ contracts/BuyMeACoffeeV2_1.sol
@@ -7,37 +7,64 @@ import "../interfaces/IPool.sol";
 import "../interfaces/IWETHGateway.sol";
 import "../interfaces/AggregatorV3Interface.sol";
 
-// deployed address: 0xE9D3AB08080d797DC6856fC476e2b9BcD778Af84
-// Goerli Testnet Contract
+/**
+ * @title BuymeAcoffee-V2.1
+ * @author Okiki Olugunna
+ * @notice This contract acts as a platform for a creator to receive donations from
+ * fans or, other words, allow their supporter to buy them a coffee.
+ * @notice When someone buys a coffee, the funds (ETH) are supplied to Aave V3 to earn
+ * interest. The owner can withdraw these donations at any time.
+ * @dev This contract is intended for the Goerli Testnet
+ * @dev The deployed contract address is 0xE9D3AB08080d797DC6856fC476e2b9BcD778Af84
+ */
 contract BuyMeACoffeeV2_1 {
-    // owner
+    /**
+     * @notice This is the owner of the contract
+     * @dev The owner is the deployer of the contract
+     */
     address payable public owner;
 
-    // AAVE Pool Address - Goerli (Pool-Proxy-Aave)
+    /**
+     * @notice This is the contract address of the Aave V3 Pool on Goerli
+     * (Pool-Proxy-Aave)
+     */
     IPool public constant AAVE_V3_POOL =
         IPool(0x368EedF3f56ad10b9bC57eed4Dac65B26Bb667f6);
-    // AAVE WETH Gateway Address - Goerli
+
+    /**
+     * @notice This is the contract address of the WETH Gateway on Goerli
+     * @dev This is where the ETH sent to the buyCoffeeWithETH() function goes through to get to the Pool
+     */
     IWETHGateway public constant WETH_GATEWAY =
         IWETHGateway(0xd5B55D3Ed89FDa19124ceB5baB620328287b915d);
 
+    /**
+     * @notice This is the contract address of the WETH 'a' token on Aave V3 (Goerli)
+     * @dev The ETH sent to Aave V3 via the WETH Gateway accrues interest in the form of 'aWETH'
+     */
     IERC20 public constant WETH_ATOKEN =
         IERC20(0x27B4692C93959048833f40702b22FE3578E77759);
 
-    // Chainlink Goerli Price Feed
-    //AggregatorV3Interface public constant ETHUSD_PRICE_FEED = AggregatorV3Interface(0x);
-
-    // Aave Goerli Oracle - returns the price of the underlying asset in the base currency
-    //address constant AAVE_ORACLE = 0x5bed0810073cc9f0DacF73C648202249E87eF6cB;
-
-    // mapping of people who donated
+    /// @notice This mapping stores the addresses of the people who donated
     mapping(address => bool) public isKind;
-    // array of people who donated
+
+    /**
+     * @notice This array stores addresses of the people donated
+     * @dev This array is used in the howMany() function to count the number of donors
+     */
     address[] public kindPeople;
 
-    // donations
+    /// @notice This state variable stores the amount of donations sent to the contract
     uint256 public ETHDonations;
 
-    // event for each time a donation is made
+    /**
+     * @notice This event gets triggered each time a donation is made
+     * @param _datetime The unix timestamp of the donation
+     * @param _tipper The address of the donor
+     * @param _name The name of the donor
+     * @param _amount The amount donated in wei
+     * @param _message The message sent with the donation
+     */
     event NewCoffeeBought(
         uint256 _datetime,
         address indexed _tipper,
@@ -46,21 +73,29 @@ contract BuyMeACoffeeV2_1 {
         string _message
     );
 
-    // event for when donations are withdrawn from contract
+    /**
+     * @notice This event gets triggered when donations are withdrawn from contract
+     * @param _datetime The unix timestamp of the withdrawal
+     * @param _amount The amount withdrawn in wei
+     */
     event WithdrawnCoffees(uint256 _datetime, uint256 _amount);
 
-    // modifier to prevent others from executing certain functions
+    /// @dev Only the owner of the contract can call functions marked by this modifier
     modifier onlyOwner() {
         require(msg.sender == owner, "You are not the owner.");
         _;
     }
 
-    // initialising the owner
+    /// @dev constructor to initialise the owner
     constructor() {
         owner = payable(msg.sender);
     }
 
-    // buy coffee with ETH
+    /**
+     * @notice This function allows supporter to "buy a coffee" with ETH
+     * @param _name The name of the donor
+     * @param _message The message they would like the contract owner to see
+     */
     function buyCoffeeWithETH(string calldata _name, string calldata _message)
         external
         payable
@@ -94,34 +129,39 @@ contract BuyMeACoffeeV2_1 {
         );
     }
 
-    // check how many people tipped
+    /**
+     * @notice This function checks how many unique addresses have bought a coffee
+     * @return uint256 The value returned is the number of unique addresses who have donated
+     */
     function howMany() external view returns (uint256) {
         return kindPeople.length;
     }
 
-    // check balance of ETH + aWETH
+    /**
+     * @notice This function checks the balance of ETH + aWETH
+     * @dev This function is not used on the frontend
+     */
     function checkBalanceETH() external view onlyOwner returns (uint256) {
         return address(this).balance + WETH_ATOKEN.balanceOf(address(this));
-
-        // could also return the usd balance using convert function with chainlink data feeds
     }
 
-    // withdraw ETH
+    /**
+     * @notice This function allows the owner to withdraw the ETH donations
+     * that have gained interest from Aave
+     */
     function withdrawETHAave() external onlyOwner {
         // making sure there is something to withdraw
         require(ETHDonations > 0, "Nothing to withdraw");
         // updating the state variable
         ETHDonations = 0;
 
-        // calculating total that will be withdrawn
-        // uint256 amountWithdrawing = address(this).balance + WETH_ATOKEN.balanceOf(address(this));
-
         // calculating the amount of aWETH earned
         uint256 aWETHBalance = WETH_ATOKEN.balanceOf(address(this));
+
         // approving the gateway to spend the aWETH
         WETH_ATOKEN.approve(address(WETH_GATEWAY), aWETHBalance);
 
-        // withdrawing the interest and ETH
+        // withdrawing the interest and ETH + sending it to the owner
         WETH_GATEWAY.withdrawETH(
             address(AAVE_V3_POOL),
             type(uint256).max,
@@ -132,30 +172,13 @@ contract BuyMeACoffeeV2_1 {
         emit WithdrawnCoffees(block.timestamp, aWETHBalance);
     }
 
-    // change address of owner
+    /**
+     * @notice This function allows the owner to change address of owner address of
+     * the contract
+     * @param _newOwner The address of the new owner
+     */
     function changeOwner(address _newOwner) external onlyOwner {
         require(_newOwner != address(0), "Cannot be a 0 address");
         owner = payable(_newOwner);
     }
-
-    /*** Chainlink Data Feeds interaction - Goerli testnet 
-    // functions to interact with chainlink data feed
-    // converting the amount of ETH to USD
-    function getConversionRate(uint256 ethAmount)
-        public
-        view
-        returns (uint256)
-    {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUSD;
-    }
-
-    // function to get the price of the ETH
-    function getPrice() public view returns (uint256) {
-        (, int256 price, , , ) = ETHUSD_PRICE_FEED.latestRoundData();
-        // converting to wei
-        return uint256(price * 1e10);
-    }
-    ***/
 }
